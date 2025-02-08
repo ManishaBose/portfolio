9b856e42e9c3c8bd3b7fefe7f4a8f2ea4fe627b5
MANISHA BOSE <manishabose.2001@gmail.com>
2025-02-08 12:59:41 +0530

    Story for Header.jsx

--- src/components/Header/Header.stories.jsx
+++ src/components/Header/Header.stories.jsx
@@ -1,12 +1,37 @@
 import { Header} from './Header';
 
+// export default {
+//   component: Header,
+// };
+
+// export const Primary = {
+//   args: {
+//     label: 'Header',
+//     primary: true,
+//   },
+// };
+// export const Mobile = {
+//     args: {
+//       label: 'Header',
+//       primary: true,
+//     },
+//   };
+
+//   import { Header } from './Header';
+
 export default {
   component: Header,
 };
 
 export const Primary = {
-  args: {
-    label: 'Header',
-    primary: true,
-  },
+    parameters: {
+        viewport: { defaultViewport: 'desktop' },
+    }
+}
+
+export const OnPhone = {
+    parameters: {
+      // 👇 Override default viewport for this story
+      viewport: { defaultViewport: 'mobile1' },
+    },
 };
