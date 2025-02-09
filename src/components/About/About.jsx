a8502a04cd03f495857e2647a0ac018c4be2e94f
MANISHA BOSE <manishabose.2001@gmail.com>
2025-02-09 23:59:31 +0530

    Made changes in the About section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,11 +1,15 @@
 export function About(){
     return(
         <div>
-            <div>Hello,</div>
-            <div>I am Manisha Bose.</div>
-            <div>I am a full-stack developer, constantly learning by building real-world projects. 
-                My goal isn’t just to write code—it’s to develop scalable applications that can impact users globally. 
-                I thrive on hands-on experience, refining my skills everyday to build efficient, high-quality solutions.
+            <div>Hello, I am Manisha</div>
+            <div>
+                I’m a full-stack developer who likes to build scalable and efficient web apps. 
+                I aim to be a one-person engineering powerhouse—capable of handling every layer of development, 
+                from crafting sleek frontend experiences to building robust backend systems and managing cloud deployments.
+            </div>
+            <div>
+                My motto? To live in the present and strive to be the best in every field I choose to work in by putting 
+                every ounce of my capability into performing at my best.
             </div>
         </div>
     )
