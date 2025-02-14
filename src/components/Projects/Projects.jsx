fcb427b91cab44496626d9e28d2f5e6ef65b2f9f
MANISHA BOSE <manishabose.2001@gmail.com>
2025-02-14 22:36:29 +0530

    Implement Projects with videos

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -4,7 +4,21 @@ export function Projects(){
     return (
         <div>
             Projects
-            <VideoCard/>
+            <div className="mt-12 mb-46 grid grid-cols-1 gap-10 sm:grid-cols-2 lg:grid-cols-3">
+                {showcase.map((showcase) => (
+                <VideoCard key={showcase.name} showcase={showcase} />
+                ))}
+            </div>
         </div>
     )
 }
+
+const showcase = [
+    {
+        name: "AI_ChatBot",
+        url: "https://github.com/ManishaBose/AI_ChatBot",
+        thumbnail: "/img/chat_bot.png",
+        video: "/projectDemo/chat_bot.mp4",
+        description: "AI Chatbot can can run on your local server",
+    },
+]
