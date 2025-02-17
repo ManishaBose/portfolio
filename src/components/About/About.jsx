a6c4e24033ffd3268f004c5e82d78b0a07a55a33
MANISHA BOSE <manishabose.2001@gmail.com>
2025-02-17 13:21:49 +0530

    Design the About section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,18 +1,13 @@
 export function About(){
     return(
-        <div>
-            <div>Hello, I am Manisha</div>
-            <div>
-                I’m a <b>full-stack developer</b> who likes to build scalable and efficient web apps. 
-                I aim to be a one-person engineering powerhouse—capable of handling every layer of development, 
-                from crafting <b>sleek frontend experiences</b> to <b>building robust backend systems</b> and <b>managing cloud deployments</b>.
-            </div>
-            <div>
-                <b>My motto?</b> To live in the present and strive to be the best in every field I choose to work in by putting 
+        <div className="text-xl text-gray-700 pb-40">
+            <div className="text-center font-bold pb-5">Hello,</div>
+            <div className="text-center font-bold text-4xl pb-10">I am Manisha.</div>
+            <div className="text-gray-800 pl-40 pr-40">
+                I’m a <b>full-stack developer</b> who likes to build <b>scalable</b> and <b>efficient</b> web apps. 
+                My motto is to live in the present and strive to be the best in every field I choose to work in by putting 
                 every ounce of my capability into performing at my best.
-            </div>
-            <button>KNOW MORE</button>
-            <hr/>
+                </div>
         </div>
     )
 }
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,7 +2,7 @@ import me from '../../assets/me.png'
 import { motion, useSpring } from "framer-motion"
 export function Hero(){
     return(
-        <div className='pt-20 pb-50'>
+        <div className='pt-10 pb-30'>
             <div className="grid grid-cols-2">
                 <div className='w-max'>
                     <div className='text-9xl'>Build.</div>
@@ -14,8 +14,10 @@ export function Hero(){
                     <img src={me} className='card-content rounded-lg'/>
                 </div>
             </div> 
-            <div className='pt-10 text-6xl'>Crafting pixel-perfect, code-  precise, scalable web application.</div>
-        <   button>DOWNLOAD RESUME</button>
+            <div className='pt-10 text-2xl text-gray-600 pb-10'>Crafting pixel-perfect, code-  precise, scalable web application.</div>
+            <a href='/cv.pdf' download>
+                <button className='bg-gray-100 p-3 rounded-full cursor-pointer text-gray-800 font-sans'>DOWNLOAD CV</button>
+            </a>
         </div>
     )
 }
